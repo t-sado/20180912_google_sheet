b02ca2b5b27ec2627b8247808d2f1b0e8d195905
sado <sado.tomoki@no1s.biz>
2018-09-12 10:44:02 +0900

    api_keyを削除

--- google_sheets.php
+++ google_sheets.php
@@ -1,6 +1,6 @@
 <?php
-	// データ取得
-	$url = 'https://sheets.googleapis.com/v4/spreadsheets/11BCnspCt2Mut3nhc4WMY6CYTd0zF9C3eCzsk1AEpKLM/values/A1:E6?key=AIzaSyDwxpicDSa3GBcLJmgE1yxdtjYpIJFogcA';
+	// データ取得 ※key=にAPI_KEYを追加して実行する
+	$url = 'https://sheets.googleapis.com/v4/spreadsheets/11BCnspCt2Mut3nhc4WMY6CYTd0zF9C3eCzsk1AEpKLM/values/A1:E6?key=';
 	mb_language('Japanese');
 	$result = json_decode(mb_convert_encoding(file_get_contents($url), "utf8", "auto"));
 
